5951ab332463cb6333833663e2de1bb75cef01df
root <root@Richard>
2024-05-15 00:43:21 +0800

    更改配置信息

--- backend/settings.py
+++ backend/settings.py
@@ -92,12 +92,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': "django.db.backends.postgresql",
+            'NAME': 'osdb',
+            'USER': 'osdb',
+            'PASSWORD': 'osdb',
+            'HOST': '127.0.0.1',
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
@@ -124,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 

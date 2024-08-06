4fd7e99bc1a00431ab14ddc224daa1758f59cd95
Richard <1273750248@qq.com>
2024-08-06 11:40:43 +0800

    新增日志查看详情功能

--- backend/settings.py
+++ backend/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'base.LogRequestMiddleware.LogRequestMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'

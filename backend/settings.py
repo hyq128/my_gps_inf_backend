455bff97d1d8e607d4f9c6e17d4004ea97826147
root <root@Richard>
2024-08-15 18:59:17 +0800

    敏感信息隐藏

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True")
 
 ALLOWED_HOSTS = [
     "gps.primedigitaltech.com",
